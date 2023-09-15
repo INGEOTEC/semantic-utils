56eb7e0d1298ceda9ffd758a5032e4e82921f5d5
Eric S. Tellez <donsadit@gmail.com>
2023-09-14 20:05:38 -0500

    synonyms

--- collocations/synonyms.jl
+++ collocations/synonyms.jl
@@ -1,6 +1,8 @@
 using Embeddings
 
 include("../metric.jl")
+include("io.jl")
+include("test.jl")
 
 function load_fasttext_embeddings(embfile)
     E = load_embeddings(FastText_Text, embfile) 
@@ -14,19 +16,12 @@ function load_fasttext_embeddings(embfile)
 end
 
 
-function similarity_synonyms(names, vocab, embeddings, dist; verbose=true, k=16, minrecall=0.95)
+function similarity_synonyms(names, vocab, embeddings, dist; verbose=true, k=12, minrecall=0.95)
     P = Dict(w => i for (i, w) in enumerate(vocab))
-    ovocab = String[]
-    ivocab = String[]
     ivocabidx = Int32[]
     for w in names
         i = get(P, w, 0)
-        if i > 0
-            push!(ivocab, w)
-            push!(ivocabidx, i)
-        else
-            push!(ovocab, w)
-        end
+        i > 0 && push!(ivocabidx, i)
     end
 
     X = StrideMatrixDatabase(embeddings)
@@ -34,31 +29,44 @@ function similarity_synonyms(names, vocab, embeddings, dist; verbose=true, k=16,
     G = create_index(dist, Y; k, minrecall, verbose)
     
     n = length(vocab)
-    R = Vector{Int32}(undef, n)
     knns, dists = searchbatch(G, X, k)
+    ivocab = vocab[ivocabidx]
 
-    (; G, vocab, ivocab, ovocab, knns, dists)
+    (; G, vocab, ivocab, ivocabidx, knns, dists)
 end
 
-function create_map(p::NamedTuple, k::Integer=4)
+function create_map(p::NamedTuple; quant=0.02)
     println("creating map")
-    map = Dict{String,Vector{String}}()
+    map = Dict{String,String}()
+    dmax = quantile(p.dists[1, :], quant)
     
     for i in 1:size(p.knns, 2)
-       I = @view p.knns[:, i]
-       map[p.vocab[i]] = p.ivocab[I[1:k]]
-    end
-
-    for w in p.ovocab
-        map[w] = [String(w)]
+       nn = p.knns[1, i]
+       dist = p.dists[1, i]
+       if dist <= dmax
+           map[p.vocab[i]] = p.ivocab[nn]
+       end
+       #I = @view p.knns[:, i]
+       #D = @view p.dist[:, i]
+       #map[p.vocab[i]] = p.ivocab[I[1:k]]
     end
 
     map
 
-    #=
-    open("map-fastText-to-evomsa-17.json", "w") do f
-           print(f, json(map, 2))
-    end
-    =#
 end
 
+
+function main(; outfile::String, quant::AbstractFloat, emb::NamedTuple, train::DataFrame, mindocs::Integer, maxndocs=0.5)
+    # train = read_json_dataframe(datafile) 
+    # emb = load_fasttext_embeddings(embeddingsfile)
+    voc = vocab(Nothing, train.text; nlist=[1], qlist=[], collocations=0, mindocs, maxndocs)
+    dataset_tokens = token(voc)
+    s = similarity_synonyms(dataset_tokens, emb.vocab, emb.embeddings, emb.dist)
+    map = create_map(s; quant)
+    
+    open(outfile, "w") do f
+           print(f, json(map, 2))
+    end
+
+    map
+end
--- collocations/test.jl
+++ collocations/test.jl
@@ -6,16 +6,16 @@ using UnicodePlots
 include("io.jl")
 include("voctrans.jl")
 
-function vocab(::Type{T}, text; nlist, qlist, collocations, mindocs, maxndoc) where {T<:Union{AbstractTokenTransformation,Nothing}}
+function vocab(::Type{T}, text; mapfile, nlist, qlist, collocations, mindocs, maxndocs) where {T<:Union{AbstractTokenTransformation,Nothing}}
     tc = TextConfig(; nlist, del_diac=true, del_punc=true)
     tt = if T === IgnoreStopwords
         sw = spanish_stopwords(tc)
         IgnoreStopwords(sw)
     elseif T === Synonyms
-        Synonyms(read_synonyms("map-fastText-to-haha.json"))
+        Synonyms(open(JSON.parse, mapfile)) #read_synonyms("map-fastText-to-haha.json"))
     elseif T === ChainTransformation
         sw = spanish_stopwords(tc)
-        ChainTransformation([IgnoreStopwords(sw), Synonyms(read_synonyms("map-fastText-to-haha.json"))])
+        ChainTransformation([IgnoreStopwords(sw), Synonyms(read_synonyms(mapfile))])
     else
         IdentityTokenTransformation()
     end
@@ -30,17 +30,34 @@ end
 vectormodel(gw::EntropyWeighting, lw, train, V) = VectorModel(gw, lw, V, train.text, train.klass)
 vectormodel(gw, lw, train, V) = VectorModel(gw, lw, V)
 
-function textclassifier(train, test; gw=EntropyWeighting(), lw=BinaryLocalWeighting(), voctype=nothing, collocations=0, nlist=[1], qlist=[], mindocs=3, maxndocs=0.5)
-    V = vocab(voctype, train.text; collocations, nlist, qlist, mindocs, maxndocs)
+function textclassifier(train, test; 
+        gw=EntropyWeighting(),
+        lw=BinaryLocalWeighting(),
+        voctype::Type=Nothing,
+        mapfile = "map-ft-delitos-0.4-3.json",
+        collocations::Integer=0,
+        nlist::Vector=[1],
+        qlist::Vector=[],
+        mindocs::Integer=3,
+        maxndocs::AbstractFloat=0.5
+    )
+
+    V = vocab(voctype, train.text; collocations, mapfile, nlist, qlist, mindocs, maxndocs)
     model = vectormodel(gw, lw, train, V)
     X, y, dim = vectorize_corpus(model, train.text), train.klass, vocsize(model)
     cls = linear_train(y, sparse(X, dim))     
     Xtest = vectorize_corpus(model, test.text)
     ypred, ypred_decision = linear_predict(cls, sparse(Xtest, dim))
-    @info json(classification_scores(test.klass, ypred), 2)
-    model
+    scores = classification_scores(test.klass, ypred)
+    @info json(scores, 2)
+    (;  gw, lw, voctype, collocations, nlist, qlist, mindocs, maxndocs,
+        vocsize=vocsize(V), trainsize=length(y), testsize=length(ypred),
+        dist=(train=countmap(y), test=countmap(test.klass), pred=countmap(ypred)),
+        scores), model
 end
  
-train = read_json_dataframe("datasets/competitions/haha2018_Es_train.json")
-test = read_json_dataframe("datasets/competitions/haha2018_Es_test.json")
+#train = read_json_dataframe("datasets/competitions/haha2018_Es_train.json")
+#test = read_json_dataframe("datasets/competitions/haha2018_Es_test.json")
+train = read_json_dataframe("datasets/datasets/delitos_ingeotec_Es_train.json")
+test = read_json_dataframe("datasets/datasets/delitos_ingeotec_Es_test.json")
 # E = totable(model, DataFrame) 
